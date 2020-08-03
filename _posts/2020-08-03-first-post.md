fb4d91289032a1d8dda159f54469c682fbe49901
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-08-03 18:07:03 +0900

    fix homepage

---
+++ _posts/2020-08-03-first-post.md
@@ -0,0 +1,11 @@
+---
+title : "test title"
+excerpt: "test이다"
+
+categories:
+    - Blog
+tags:
+    - Blog
+---
+
+깃헙 첫 글이다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "test title",
+        "excerpt":"깃헙 첫 글이다.  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "http://localhost:4000/blog/first-post/",
+        "teaser": "http://localhost:4000/assets/images/sample.jpeg"
+      }]
