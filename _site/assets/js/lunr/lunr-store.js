1bed3dd804271f16a6296f7286fc200a9b604b7d
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-08-04 22:51:00 +0900

    fix post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "test title",
-        "excerpt":"깃헙 첫 글이다.  ","categories": ["Blog"],
+        "excerpt":"깃헙 첫 글이다. 일단 커밋을 채워보자  ","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/first-post/",
         "teaser": "http://localhost:4000/assets/images/sample.jpeg"
