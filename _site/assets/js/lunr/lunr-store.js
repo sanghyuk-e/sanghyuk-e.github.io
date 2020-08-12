fccc4b90f8977c70df4af4420d12cd34c6ebcb84
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-08-12 18:39:43 +0900

    fix post address

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -14,6 +14,6 @@ var store = [{
         "title": "[git] 버전 관리 무시 목록(.gitignore) 설정",
         "excerpt":".gitignore 1. 위험성 특정 파일은 git에서 버전 관리 대상에서 제외해야 합니다. 프로젝트를 업로드할 때 개인키가 프로젝트 폴더내에 있을 수 있고, 이런 중요한 설정 정보는 git 저장소에 추가하지 말아야 합니다. 이로인해서 해킹에 대상이 될 수 있고, 심각한 피해를 입을 수 있습니다. 2. 해결방안 git 에는 이러한 문제를 방지하기 위한 무시 목록을...","categories": ["git"],
         "tags": ["git"],
-        "url": "http://localhost:4000/git/3/",
+        "url": "http://localhost:4000/git/1/",
         "teaser": "http://localhost:4000/assets/images/sample.jpeg"
       }]
