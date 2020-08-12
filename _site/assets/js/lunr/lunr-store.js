7a09177720462b17814c16c69a736f7043770ccc
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-08-12 18:35:22 +0900

    fix post name

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,13 +2,13 @@ var store = [{
         "title": "test title",
         "excerpt":"깃헙 첫 글이다.   일단 커밋을 채워보자   커밋 수정하는 법을 알아본다  ","categories": ["Blog"],
         "tags": ["Blog"],
-        "url": "http://localhost:4000/blog/first-post/",
+        "url": "http://localhost:4000/blog/1/",
         "teaser": "http://localhost:4000/assets/images/sample.jpeg"
       },{
         "title": "두번째 글",
         "excerpt":"두번째 글 입니다.   안녕하세요   ","categories": ["Blog"],
         "tags": ["Blog"],
-        "url": "http://localhost:4000/blog/1/",
+        "url": "http://localhost:4000/blog/2/",
         "teaser": "http://localhost:4000/assets/images/sample.jpeg"
       },{
         "title": "[git] 버전 관리 무시 목록(.gitignore) 설정",
