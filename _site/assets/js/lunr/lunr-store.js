512269900d0ee552853c2437196ffeab2563847f
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-09-02 18:16:15 +0900

    fix

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,17 +3,17 @@ var store = [{
         "excerpt":"블로그 제대로 된 블로그를 만들어보자는 생각을 가지고서 몇 번이나 블로그를 만들었지만 매번 얼마 지나지 않아 더 이상 글을 올리지 않게 되었습니다. 이유는 여러 가지지만 가장 큰 이유는 ‘방법을 몰라서’ 였던 것 같습니다. 좋은 기술 블로그 업무를 하면서 많은 블로그들을 봅니다. 글을 잘 작성하고 정리가 잘 되어 보기 편한 블로그가 있다면...","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/1/",
-        "teaser": "http://localhost:4000/assets/images/sample.jpeg"
+        "teaser": null
       },{
         "title": "이력서 작성하기",
         "excerpt":"이력서 이직을 하기 위해서 계속 이력서를 작성해왔다. 채용 사이트인 원티드의 양식을 이용하여 가장 기본적인 내용들을 작성하였고 내용은 계속해서 수정하는 방식으로 작성했다. 이렇게 작성한 이력서를 통해서 많은 곳에 지원서를 넣어보았고, 합격을 하기도 했다. 당연하게도 불합격도 있었고 그 수는 적은편이 아니었다고 생각한다. 요즘에는 이력서는 엉망이 아니라면 패스시키고, 코딩테스트로 변별력을 확보하려는 회사가 많은...","categories": ["Resume"],
         "tags": ["Resume"],
         "url": "http://localhost:4000/resume/1/",
-        "teaser": "http://localhost:4000/assets/images/sample.jpeg"
+        "teaser": null
       },{
         "title": "[git] 버전 관리 무시 목록(.gitignore) 설정",
         "excerpt":".gitignore 1. 위험성 특정 파일은 git에서 버전 관리 대상에서 제외해야 합니다. 프로젝트를 업로드할 때 개인키가 프로젝트 폴더내에 있을 수 있고, 이런 중요한 설정 정보는 git 저장소에 추가하지 말아야 합니다. 이로인해서 해킹에 대상이 될 수 있고, 심각한 피해를 입을 수 있습니다. 2. 해결방안 git 에는 이러한 문제를 방지하기 위한 무시 목록을...","categories": ["git"],
         "tags": ["git"],
         "url": "http://localhost:4000/git/1/",
-        "teaser": "http://localhost:4000/assets/images/sample.jpeg"
+        "teaser": null
       }]
