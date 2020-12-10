cf57a509808c83050eae431fcdbb2b1523b59c08
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-12-10 19:44:27 +0900

    add teaser

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -15,31 +15,31 @@ var store = [{
         "excerpt":".gitignore 1. 위험성 특정 파일은 git에서 버전 관리 대상에서 제외해야 합니다. 프로젝트를 업로드할 때 개인키가 프로젝트 폴더내에 있을 수 있고, 이런 중요한 설정 정보는 git 저장소에 추가하지 말아야 합니다. 이로인해서 해킹에 대상이 될 수 있고, 심각한 피해를 입을 수 있습니다. 2. 해결방안 git 에는 이러한 문제를 방지하기 위한 무시 목록을...","categories": ["git"],
         "tags": ["git"],
         "url": "http://localhost:4000/git/1/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Git/github.png"
       },{
         "title": "CHAPTER01: 스위프트 - 1.스위프트 언어의 탄생과 배경",
         "excerpt":"1. 스위프트 언어의 탄생과 배경 스위프트(Swift)는 애플이 2014 세계 개발자 대회(WWDC)에서 발표한 iOS나 macOS 앱 개발의 새로운 언어이다. 그동안 앱을 개발하는 데에 사용되던 오브젝티브-C를 대체할 목적으로 발표되었다. 애플의 이전 주력 언어였던 오프젝티브-C는 C언어를 기초로 하여 스몰토크의 메시지 전달 개념과 객체지향 개념을 반영한, 객체지향형 C 언어이다. C 언어의 장점이자 단점인 포인터...","categories": ["Swift-Grammar"],
         "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
         "url": "http://localhost:4000/swift-grammar/1-1/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER01: 스위프트 - 2.스위프트 언어의 특징",
         "excerpt":"2. 스위프트 언어의 특징 상대적으로 언어에 대한 진입장벽이 낮고 개발하기 쉬운 자바스크립트나 파이썬, 루비와 같은 동적 바인딩 타입의 언어는 생산성이 좋지만 성능이 부족한 경우가 많고, 성능이 좋은 언어일수록 진입 장벽이 높고 개발하기 어려워 생산성이 낮은 경우가 많다. 이에 비해 스위프트는 개발 생산성과 앱 성능 모두에서 만족할 만한 결과를 가져오는 언어로...","categories": ["Swift-Grammar"],
         "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
         "url": "http://localhost:4000/swift-grammar/1-2/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER01: 스위프트 - 3. 오브젝티브-C vs 스위프트",
         "excerpt":"3. 오브젝티브-C vs 스위프트 오브젝티브-C는 20여 년 동안 애플의 주력 개발 언어로 사용된 만큼 튼튼한 아키텍처와 검증된 성능을 가지고 있다. 이런 오브젝티브-C를 대체할 목적으로 스위프트가 출현했을 때 많은 사람의 관심은 과연 스위프트가 오브젝티브-C를 대체할 수 있을까였다. 언어를 대체한다는 것은 기존 언어가 담당하던 모든 역할을 처리할 수 있는 역량이 되어야 가능하다....","categories": ["Swift-Grammar"],
         "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
         "url": "http://localhost:4000/swift-grammar/1-3/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER02: Xcode - 1. 통합개발 환경",
         "excerpt":"이번에 다룰 내용은 툴(Tool)이다. 프로그래밍 환경에서 툴이 차지하는 비중은 상당하다. 얼마나 빠르고 쉽게 개발할 수 있는지가 툴에 달려 있기 때문이다. 그래서 툴에 대해 자세히 알고, 마음대로 다룰 수 있도록 능력을 갖추는 것은 매우 중요하다. 현대 프로그래밍은 상당히 많은 부분을 툴에 의존하는 경향이 있지만, 이는 툴과 프로그래밍 개발이 이제 떼려야 뗄...","categories": ["Swift-Grammar"],
         "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
         "url": "http://localhost:4000/swift-grammar/2-1/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "Single Sign On (SSO)",
         "excerpt":"Single Sign On (SSO) 한 번의 로그인으로 여러 개의 서비스들을 이용할 수 있게 해주는 시스템 SSO(Single Sign On)는 가장 기본적인 인증 시스템이다. ‘모든 인증을 하나의 시스템에서’라는 목적하에 개발된 것이다. 즉 시스템이 몇 대가 되어도 하나의 시스템에서 인증에 성공하면 다른 시스템에 대한 접근 권한도 모두 얻는 것이다. SK텔레콤의 연계 사이트에서는 하나의...","categories": ["Development"],
@@ -51,13 +51,13 @@ var store = [{
         "excerpt":"2. Xcode란? Xcode는 앞으로 우리가 인터넷 브라우저만큼이나 자유자재로 다루어야 할 툴로서, iOS와 macOS용 앱을 개발할 수 있도록 애플에서 제공하는 IDE이다. ‘엑스코드’라고 읽으며, 맥북 등 애플 컴퓨터의 운영체제인 맥 OS에서만 실행되는 애플리케이션이다. 우리는 이 Xcode를 이용하여 오브젝티브-C나 스위프트를 작성하고, 애플리케이션을 개발한다. 일반적으로 C 기반의 코드를 컴파일할 때 많이 사용되는 GCC라는 컴파일러가...","categories": ["Swift-Grammar"],
         "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
         "url": "http://localhost:4000/swift-grammar/2-2/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER02: Xcode - 3. Xcode 설치",
         "excerpt":"3. Xcode 설치 Xcode는 애플 전용 데스크톱 운영체제인 macOS에서만 실행할 수 있다. 따라서 애플에서 출시한 맥북(MacBook), 맥북 프로(MacBook Pro), 맥북 에어(MacBook Air), 아이맥(iMac), 맥 미니(Mac Mini), 맥 프로(Mac Pro) 중 하나가 필요하다. 마이크로소프트사의 Windows 운영체제가 설치된 일반 PC에서는 Xcode를 설치할 수 없다. 과거에는 Xcode를 내려받기 위해서 맥 OS 개발자 프로그램이나...","categories": ["Swift-Grammar"],
         "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
         "url": "http://localhost:4000/swift-grammar/2-3/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "Swift 개발자 면접 질문 List",
         "excerpt":"Swift 개발자 면접 질문 List 개발자를 채용하는 면접에는 기술 면접이 있다. 여러번 면접을 경험하다 보니 특정 질문들을 자주 받는 느낌이 들었다. 업무를 진행하는데 있어서 중요한 부분들을 주로 질문한다고 생각이 들었고, 그 말은 반대로 꼭 공부해야 알아야 할 부분이라는 생각이 들었다. 그래서 실제 면접을 통해서 받는 질문들을 정리하려고 한다. 우선 나는...","categories": ["Interview"],
@@ -69,7 +69,7 @@ var store = [{
         "excerpt":"MacOS를 업데이트를 할 때마다 여러 문제가 발생하는데, 이번에도 개발 관련 이슈가 터졌다. 매번 구글에 검색하여 해결했는데 이번에는 블로그에 간단하게 정리하려고 한다. 깃에 커밋을 하려고 디렉토리로 이동을 했는데 왠지 모르게 어색한 느낌이 들었다. 자세히 보니 터미널에 브랜치 표시도 나오지 않았고 해당 디렉토리의 현재 상태도 보이지 않았다. 아니나 다를까 깃에 문제가 생겨있었다....","categories": ["git"],
         "tags": ["Swift","git","terminal","error"],
         "url": "http://localhost:4000/git/2/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Git/github.png"
       },{
         "title": "Mac 업데이트 후 CocoaPod 오류 해결방법",
         "excerpt":"MacOS를 업데이트를 할 때마다 여러 문제가 발생하는데, xcrun error를 해결하고 나니 CocoaPods에서 다른 오류가 발생했다. CocoaPods로 새로운 라이브러리를 추가하려고 install 을 하려는데 다음과 같은 오류 메시지가 등장했다. 처음 보는 오류 메시지에 당연하게도 구글에 검색을 했고, 해결 방법을 찾을 수 있었다. Brew based CocoaPods CLI fails in MacOS 10.15 Catalina due...","categories": ["iOS"],
@@ -81,5 +81,5 @@ var store = [{
         "excerpt":"Firbase란? 구글에서 제공하는 Firebase라는 플랫폼은 여러가지 기능들을 제공하고 있어 많은 개발자가 사용하고 있다. 제공하는 기능은 인증, 데이터베이스, 푸시 알람, API 등이 있으며 복잡한 과정이 필요하지 않다는 것이 장점이다. Firebase 시작하기 기존 블로그들에 안드로이드를 기반으로 작성된 글이 매우 많다. iOS를 기반으로 작성된 글이 상대적으로 적은데, 연습하는 동시에 최신 과정을 한번 작성해보려고...","categories": ["Firebase"],
         "tags": ["Firebase","Database","cloud"],
         "url": "http://localhost:4000/firebase/1/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/posts/Firebase/firebase.png"
       }]
