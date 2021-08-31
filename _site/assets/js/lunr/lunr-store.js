2164ab761f6135034a6bb20b74d9210c3001afba
sanghyuk-e <sanghyuk.eee@gmail.com>
2021-09-01 01:49:36 +0900

    fix comments system

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,115 +1,145 @@
 var store = [{
         "title": "좋은 블로그",
         "excerpt":"블로그 제대로 된 블로그를 만들어보자는 생각을 가지고서 몇 번이나 블로그를 만들었지만 매번 얼마 지나지 않아 더 이상 글을 올리지 않게 되었다. 이유는 여러 가지지만 가장 큰 이유는 ‘방법을 몰라서’ 였던 것 같다. 좋은 기술 블로그 업무를 하면서 많은 블로그들을 본다. 글을 잘 작성하고 정리가 잘 되어 보기 편한 블로그가 있다면...","categories": ["Blog"],
-        "tags": ["Blog"],
-        "url": "http://localhost:4000/blog/1/",
-        "teaser": null
-      },{
-        "title": "좋은 개발자의 좋은 이력서 작성하기",
-        "excerpt":"이력서 이직을 하기 위해서 계속 이력서를 작성해왔다. 채용 사이트인 원티드의 양식을 이용하여 가장 기본적인 내용들을 작성하였고 내용은 계속해서 수정하는 방식으로 작성했다. 이렇게 작성한 이력서를 통해서 많은 곳에 지원서를 넣어보았고, 합격을 하기도 했다. 당연하게도 불합격도 있었고 그 수는 적은편이 아니었다고 생각한다. 요즘에는 이력서는 정말 엉망이 아니라면 패스시키고, 코딩테스트로 변별력을 확보하려는 회사가...","categories": ["Resume"],
-        "tags": ["Resume"],
-        "url": "http://localhost:4000/resume/1/",
+        "tags": ["Blog","블로그","글쓰기"],
+        "url": "https://sanghyuk.dev/blog/1/",
         "teaser": null
       },{
         "title": "[git] 버전 관리 무시 목록(.gitignore) 설정",
-        "excerpt":".gitignore 1. 위험성 특정 파일은 git에서 버전 관리 대상에서 제외해야 합니다. 프로젝트를 업로드할 때 개인키가 프로젝트 폴더내에 있을 수 있고, 이런 중요한 설정 정보는 git 저장소에 추가하지 말아야 합니다. 이로인해서 해킹에 대상이 될 수 있고, 심각한 피해를 입을 수 있습니다. 2. 해결방안 git 에는 이러한 문제를 방지하기 위한 무시 목록을...","categories": ["git"],
-        "tags": ["git"],
-        "url": "http://localhost:4000/git/1/",
-        "teaser": "http://localhost:4000/assets/images/posts/Git/github.png"
+        "excerpt":".gitignore 1. 위험성 특정 파일은 git에서 버전 관리 대상에서 제외해야 한다 프로젝트를 업로드할 때 개인키가 프로젝트 폴더내에 있을 수 있고, 이런 중요한 설정 정보는 git 저장소에 추가하지 말아야 한다. 이로인해서 해킹에 대상이 될 수 있고, 심각한 피해를 입을 수 있다. 2. 해결방안 git 에는 이러한 문제를 방지하기 위한 무시 목록을...","categories": ["git"],
+        "tags": ["git","github","gitignore","깃","깃헙"],
+        "url": "https://sanghyuk.dev/git/1/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Git/github.png"
       },{
         "title": "CHAPTER01: 스위프트 - 1.스위프트 언어의 탄생과 배경",
         "excerpt":"1. 스위프트 언어의 탄생과 배경 스위프트(Swift)는 애플이 2014 세계 개발자 대회(WWDC)에서 발표한 iOS나 macOS 앱 개발의 새로운 언어이다. 그동안 앱을 개발하는 데에 사용되던 오브젝티브-C를 대체할 목적으로 발표되었다. 애플의 이전 주력 언어였던 오프젝티브-C는 C언어를 기초로 하여 스몰토크의 메시지 전달 개념과 객체지향 개념을 반영한, 객체지향형 C 언어이다. C 언어의 장점이자 단점인 포인터...","categories": ["Swift-Grammar"],
-        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/1-1/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","Swift","스위프트","iOS"],
+        "url": "https://sanghyuk.dev/swift-grammar/1-1/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER01: 스위프트 - 2.스위프트 언어의 특징",
         "excerpt":"2. 스위프트 언어의 특징 상대적으로 언어에 대한 진입장벽이 낮고 개발하기 쉬운 자바스크립트나 파이썬, 루비와 같은 동적 바인딩 타입의 언어는 생산성이 좋지만 성능이 부족한 경우가 많고, 성능이 좋은 언어일수록 진입 장벽이 높고 개발하기 어려워 생산성이 낮은 경우가 많다. 이에 비해 스위프트는 개발 생산성과 앱 성능 모두에서 만족할 만한 결과를 가져오는 언어로...","categories": ["Swift-Grammar"],
-        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/1-2/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","Swift","스위프트","iOS"],
+        "url": "https://sanghyuk.dev/swift-grammar/1-2/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER01: 스위프트 - 3. 오브젝티브-C vs 스위프트",
         "excerpt":"3. 오브젝티브-C vs 스위프트 오브젝티브-C는 20여 년 동안 애플의 주력 개발 언어로 사용된 만큼 튼튼한 아키텍처와 검증된 성능을 가지고 있다. 이런 오브젝티브-C를 대체할 목적으로 스위프트가 출현했을 때 많은 사람의 관심은 과연 스위프트가 오브젝티브-C를 대체할 수 있을까였다. 언어를 대체한다는 것은 기존 언어가 담당하던 모든 역할을 처리할 수 있는 역량이 되어야 가능하다....","categories": ["Swift-Grammar"],
-        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/1-3/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","Objective-C","Swift","스위프트","오브젝티브-C","iOS"],
+        "url": "https://sanghyuk.dev/swift-grammar/1-3/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER02: Xcode - 1. 통합개발 환경",
         "excerpt":"이번에 다룰 내용은 툴(Tool)이다. 프로그래밍 환경에서 툴이 차지하는 비중은 상당하다. 얼마나 빠르고 쉽게 개발할 수 있는지가 툴에 달려 있기 때문이다. 그래서 툴에 대해 자세히 알고, 마음대로 다룰 수 있도록 능력을 갖추는 것은 매우 중요하다. 현대 프로그래밍은 상당히 많은 부분을 툴에 의존하는 경향이 있지만, 이는 툴과 프로그래밍 개발이 이제 떼려야 뗄...","categories": ["Swift-Grammar"],
-        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/2-1/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","Xcode","엑스코드","IDE"],
+        "url": "https://sanghyuk.dev/swift-grammar/2-1/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "Single Sign On (SSO)",
         "excerpt":"Single Sign On (SSO) 한 번의 로그인으로 여러 개의 서비스들을 이용할 수 있게 해주는 시스템 SSO(Single Sign On)는 가장 기본적인 인증 시스템이다. ‘모든 인증을 하나의 시스템에서’라는 목적하에 개발된 것이다. 즉 시스템이 몇 대가 되어도 하나의 시스템에서 인증에 성공하면 다른 시스템에 대한 접근 권한도 모두 얻는 것이다. SK텔레콤의 연계 사이트에서는 하나의...","categories": ["Development"],
-        "tags": ["Development"],
-        "url": "http://localhost:4000/development/1/",
+        "tags": ["Development","개발","개발자","SSO"],
+        "url": "https://sanghyuk.dev/development/1/",
         "teaser": null
       },{
         "title": "CHAPTER02: Xcode - 2. Xcode란?",
         "excerpt":"2. Xcode란? Xcode는 앞으로 우리가 인터넷 브라우저만큼이나 자유자재로 다루어야 할 툴로서, iOS와 macOS용 앱을 개발할 수 있도록 애플에서 제공하는 IDE이다. ‘엑스코드’라고 읽으며, 맥북 등 애플 컴퓨터의 운영체제인 맥 OS에서만 실행되는 애플리케이션이다. 우리는 이 Xcode를 이용하여 오브젝티브-C나 스위프트를 작성하고, 애플리케이션을 개발한다. 일반적으로 C 기반의 코드를 컴파일할 때 많이 사용되는 GCC라는 컴파일러가...","categories": ["Swift-Grammar"],
-        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/2-2/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","Xcode","엑스코드"],
+        "url": "https://sanghyuk.dev/swift-grammar/2-2/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER02: Xcode - 3. Xcode 설치",
         "excerpt":"3. Xcode 설치 Xcode는 애플 전용 데스크톱 운영체제인 macOS에서만 실행할 수 있다. 따라서 애플에서 출시한 맥북(MacBook), 맥북 프로(MacBook Pro), 맥북 에어(MacBook Air), 아이맥(iMac), 맥 미니(Mac Mini), 맥 프로(Mac Pro) 중 하나가 필요하다. 마이크로소프트사의 Windows 운영체제가 설치된 일반 PC에서는 Xcode를 설치할 수 없다. 과거에는 Xcode를 내려받기 위해서 맥 OS 개발자 프로그램이나...","categories": ["Swift-Grammar"],
-        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/2-3/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","Xcode","엑스코드"],
+        "url": "https://sanghyuk.dev/swift-grammar/2-3/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER02: Xcode - 4. Xcode 실행하기",
         "excerpt":"4. Xcode 실행하기 설치가 끝난 Xcode를 실행해 보자. 실행하는 방법은 간단하다. 맥 하단의 독(Dock)에서 응용프로그램(Launchpad) 아이콘을 누르면 앱 목록이 나타나는데, 이 중에서 Xocde 아이콘을 찾아 클릭하면 된다. Xcode를 처음 실행했을 때는 아래 그림처럼 추가 컴포넌트 설치를 요구하는 창이 나타날 수 있는데, 이때는 Install을 클릭하여 새로 추가된 컴포넌트들을 설치한다. 만약 Quit을...","categories": ["Swift-Grammar"],
-        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/2-4/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","Xcode","엑스코드"],
+        "url": "https://sanghyuk.dev/swift-grammar/2-4/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "CHAPTER02: Xcode - 5. Xcode의 구성 요소",
         "excerpt":"5. Xcode의 구성 요소 프로젝트를 생성하거나 생성된 프로젝트를 실행하면 Xcode는 여러 개의 작은 영역으로 분할된 커다란 윈도우 형태로 나타난다. ‘워크 스페이스(Work Space)’라고 불리는 이 윈도우는 개발에 관련된 모든 것을 처리하기 위해 만들어진 Xcode의 가장 기본적인 작업 공간이다. 워크스페이슨는 기능에 따라 여러 개의 작은 영역으로 다시 분할되는데, 각각의 영역은 저마다 고유한...","categories": ["Swift-Grammar"],
         "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4"],
-        "url": "http://localhost:4000/swift-grammar/2-5/",
-        "teaser": "http://localhost:4000/assets/images/posts/Swift-Grammar/swift.png"
-      },{
-        "title": "Swift 개발자 면접 질문 List",
-        "excerpt":"Swift 개발자 면접 질문 List 개발자를 채용하는 면접에는 기술 면접이 있다. 여러번 면접을 경험하다 보니 특정 질문들을 자주 받는 느낌이 들었다. 업무를 진행하는데 있어서 중요한 부분들을 주로 질문한다고 생각이 들었고, 그 말은 반대로 꼭 공부해야 알아야 할 부분이라는 생각이 들었다. 그래서 실제 면접을 통해서 받는 질문들을 정리하려고 한다. 우선 나는...","categories": ["Interview"],
-        "tags": ["Interview"],
-        "url": "http://localhost:4000/interview/1/",
-        "teaser": null
+        "url": "https://sanghyuk.dev/swift-grammar/2-5/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
       },{
         "title": "[codeit - 컴퓨터 개론] 프로그래밍 시작하기 in Python (Part. 1)",
-        "excerpt":"프로그래밍 시작하기 in Python 프로그래밍 시장은 계속 커지고 있다. 하지만 사실 현업에서는 뛰어난 개발자를 없어서 못 구한다는 것. 프로그래밍의 세계는 광범위하기 때문에, 올바른 방향을 갖고 효과적으로 공부하는 것이 매우 중요하다. 코드잇의 컴퓨터 개론을 통해 프로그래밍 언어, 프로그래머, 프로그램에 대해 올바른 이해를 갖고, 나만의 커리어 로드맵을 만들어 보자. 컴퓨터의 세계를 이해하려면...","categories": ["codeit"],
-        "tags": ["Python","codeit","programming"],
-        "url": "http://localhost:4000/codeit/1/",
-        "teaser": "http://localhost:4000/assets/images/posts/Codeit/codeit.png"
+        "excerpt":"컴퓨터 개론 프로그래밍 시장은 계속 커지고 있다. 하지만 사실 현업에서는 뛰어난 개발자를 없어서 못 구한다는 것. 프로그래밍의 세계는 광범위하기 때문에, 올바른 방향을 갖고 효과적으로 공부하는 것이 매우 중요하다. 코드잇의 컴퓨터 개론을 통해 프로그래밍 언어, 프로그래머, 프로그램에 대해 올바른 이해를 갖고, 나만의 커리어 로드맵을 만들어 보자. 💡Infomation: 컴퓨터 개론 학습은 하루...","categories": ["codeit"],
+        "tags": ["Python","codeit","programming","tutorial","파이썬","튜토리얼","개발"],
+        "url": "https://sanghyuk.dev/codeit/1/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Codeit/codeit.png"
       },{
         "title": "[codeit - 컴퓨터 개론] 프로그래밍 시작하기 in Python (Part. 2)",
-        "excerpt":"프로그래밍 시작하기 in Python” 프로그래밍 시장은 계속 커지고 있다. 하지만 사실 현업에서는 뛰어난 개발자를 없어서 못 구한다는 것. 프로그래밍의 세계는 광범위하기 때문에, 올바른 방향을 갖고 효과적으로 공부하는 것이 매우 중요하다. 코드잇의 컴퓨터 개론을 통해 프로그래밍 언어, 프로그래머, 프로그램에 대해 올바른 이해를 갖고, 나만의 커리어 로드맵을 만들어 보자. 컴퓨터의 세계를 이해하려면...","categories": ["codeit"],
-        "tags": ["Python","codeit","programming"],
-        "url": "http://localhost:4000/codeit/2/",
-        "teaser": "http://localhost:4000/assets/images/posts/Codeit/codeit.png"
+        "excerpt":"컴퓨터 개론 프로그래밍 시장은 계속 커지고 있다. 하지만 사실 현업에서는 뛰어난 개발자를 없어서 못 구한다는 것. 프로그래밍의 세계는 광범위하기 때문에, 올바른 방향을 갖고 효과적으로 공부하는 것이 매우 중요하다. 코드잇의 컴퓨터 개론을 통해 프로그래밍 언어, 프로그래머, 프로그램에 대해 올바른 이해를 갖고, 나만의 커리어 로드맵을 만들어 보자. 💡Infomation: 컴퓨터 개론 학습은 하루...","categories": ["codeit"],
+        "tags": ["Python","codeit","programming","tutorial","파이썬","튜토리얼","개발"],
+        "url": "https://sanghyuk.dev/codeit/2/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Codeit/codeit.png"
       },{
         "title": "Mac 업데이트 후 xcrun error 해결방법",
         "excerpt":"MacOS를 업데이트를 할 때마다 여러 문제가 발생하는데, 이번에도 개발 관련 이슈가 터졌다. 매번 구글에 검색하여 해결했는데 이번에는 블로그에 간단하게 정리하려고 한다. 깃에 커밋을 하려고 디렉토리로 이동을 했는데 왠지 모르게 어색한 느낌이 들었다. 자세히 보니 터미널에 브랜치 표시도 나오지 않았고 해당 디렉토리의 현재 상태도 보이지 않았다. 아니나 다를까 깃에 문제가 생겨있었다....","categories": ["git"],
-        "tags": ["Swift","git","terminal","error"],
-        "url": "http://localhost:4000/git/2/",
-        "teaser": "http://localhost:4000/assets/images/posts/Git/github.png"
+        "tags": ["Swift","git","terminal","error","xcrun","깃","깃헙","에러","터미널"],
+        "url": "https://sanghyuk.dev/git/2/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Git/github.png"
       },{
         "title": "Mac 업데이트 후 CocoaPod 오류 해결방법",
         "excerpt":"MacOS를 업데이트를 할 때마다 여러 문제가 발생하는데, xcrun error를 해결하고 나니 CocoaPods에서 다른 오류가 발생했다. CocoaPods로 새로운 라이브러리를 추가하려고 install 을 하려는데 다음과 같은 오류 메시지가 등장했다. 처음 보는 오류 메시지에 당연하게도 구글에 검색을 했고, 해결 방법을 찾을 수 있었다. Brew based CocoaPods CLI fails in MacOS 10.15 Catalina due...","categories": ["iOS"],
-        "tags": ["Swift","iOS","terminal","error"],
-        "url": "http://localhost:4000/ios/1/",
+        "tags": ["Swift","iOS","terminal","error","CocoaPod","코코아팟","에러"],
+        "url": "https://sanghyuk.dev/ios/1/",
         "teaser": null
       },{
         "title": "Firebase 시작하기",
         "excerpt":"Firbase란? 구글에서 제공하는 Firebase라는 플랫폼은 여러가지 기능들을 제공하고 있어 많은 개발자가 사용하고 있다. 제공하는 기능은 인증, 데이터베이스, 푸시 알람, API 등이 있으며 복잡한 과정이 필요하지 않다는 것이 장점이다. Firebase 시작하기 기존 블로그들에 안드로이드를 기반으로 작성된 글이 매우 많다. iOS를 기반으로 작성된 글이 상대적으로 적은데, 연습하는 동시에 최신 과정을 한번 작성해보려고...","categories": ["Firebase"],
-        "tags": ["Firebase","Database","cloud"],
-        "url": "http://localhost:4000/firebase/1/",
-        "teaser": "http://localhost:4000/assets/images/posts/Firebase/firebase.png"
+        "tags": ["Firebase","Database","cloud","tutorial","파이어베이스","데이터베이스","클라우드","튜토리얼"],
+        "url": "https://sanghyuk.dev/firebase/1/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Firebase/firebase.png"
+      },{
+        "title": "[codeit - 컴퓨터 개론] 프로그래밍 언어 이해하기 (Part. 1)",
+        "excerpt":"컴퓨터 개론 프로그래밍 시장은 계속 커지고 있다. 하지만 사실 현업에서는 뛰어난 개발자를 없어서 못 구한다는 것. 프로그래밍의 세계는 광범위하기 때문에, 올바른 방향을 갖고 효과적으로 공부하는 것이 매우 중요하다. 코드잇의 컴퓨터 개론을 통해 프로그래밍 언어, 프로그래머, 프로그램에 대해 올바른 이해를 갖고, 나만의 커리어 로드맵을 만들어 보자. 💡Infomation: 컴퓨터 개론 학습은 하루...","categories": ["codeit"],
+        "tags": ["Python","codeit","programming","tutorial","파이썬","튜토리얼","개발"],
+        "url": "https://sanghyuk.dev/codeit/3/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Codeit/codeit.png"
       },{
         "title": "Jekyll 블로그 검색엔진에 등록하기 - Naver",
         "excerpt":"Jekyll 블로그 검색엔진에 등록하기 - NAVER 블로그에 작성한 글이 검색엔진에 노출시키기 위해서는 블로그를 등록하는 과정이 필요하다. 이번에는 Jekyll로 작성된 블로그가 네이버에서 검색되도록 설정하는 과정이다. 네이버 웹 마스터 도구에 속성 추가 네이버 서치어드바이저에 접속한다. 로그인을 진행하는데 Naver 계정이 필요하디. (대한민국 사람이면 없을리가 없다.) 웹 마스터 도구로 접속한다. 사이트 등록 란에 자신의...","categories": ["Blog"],
-        "tags": ["jekyll","blog","search engine"],
-        "url": "http://localhost:4000/blog/1/",
-        "teaser": "http://localhost:4000/assets/images/posts/Blog/Jekyll.jpeg"
+        "tags": ["jekyll","blog","search engine","naver","지킬","블로그","네이버","검색"],
+        "url": "https://sanghyuk.dev/blog/2/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Blog/Jekyll.jpeg"
+      },{
+        "title": "Jekyll 블로그 검색엔진에 등록하기 - Google",
+        "excerpt":"Jekyll 블로그 검색엔진에 등록하기 - Google 블로그에 작성한 글이 검색엔진에 노출시키기 위해서는 블로그를 등록하는 과정이 필요하다. 이번에는 Jekyll로 작성된 블로그가 구글에서 검색되도록 설정하는 과정이다. 구글 Search Console에 속성 추가 구글 Search Console에 접속한다. 로그인을 진행하는데 Google 계정이 필요하디. 사이트 등록 란에 자신의 jekyll 블로그 주소를 입력하여 추가한다. (ex.http://[github 사용자명].github.io/ 또는...","categories": ["Blog"],
+        "tags": ["jekyll","blog","search engine","google","지킬","블로그","구글","검색"],
+        "url": "https://sanghyuk.dev/blog/3/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Blog/Jekyll.jpeg"
+      },{
+        "title": "iOS 면접 질문 모음",
+        "excerpt":"iOS 면접 질문 모음 개발자를 채용하는 면접에는 기술 면접이 있다. 여러번 면접을 경험하다 보니 특정 질문들을 자주 받는 느낌이 들었다. 업무를 진행하는데 있어서 중요한 부분들을 주로 질문한다고 생각이 들었고, 그 말은 반대로 꼭 공부해야 알아야 할 부분이라는 생각이 들었다. 그래서 실제 면접을 통해서 받는 질문들을 정리하려고 한다. 관련 자료들을 찾던...","categories": ["iOS"],
+        "tags": ["interview","question","programming","면접","면접 질문","swift","인터뷰","질문","예상문제","이력서"],
+        "url": "https://sanghyuk.dev/ios/2/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/iOS/2021-02-01-2/interview.png"
+      },{
+        "title": "CHAPTER06: 옵셔널 - 1. 옵셔널 타입 선언과 정의",
+        "excerpt":"옵셔널(Optional)은 스위프트에서 도입된 새로운 개념으로서 언어 차원에서 프로그램의 안정성을 높이기 위해 사용하는 개념이다. 옵셔널의 개념을 한 문장으로 정의하자면 ‘nil을 사용할 수 있는 타입과 사용할 수 없는 타입을 구분하고, 사용할 수 있는 타입을 가리켜 옵셔널 타입(Optional Type)이라고 부른다’라고 할 수 있다. 여기서 말하는 nil이란, 값이 없음을 의미하는 특수한 값이다. 정수형의 0이나,...","categories": ["Swift-Grammar"],
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","옵셔널"],
+        "url": "https://sanghyuk.dev/swift-grammar/6-1/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
+      },{
+        "title": "CHAPTER06: 옵셔널 - 2. 옵셔널 값 처리",
+        "excerpt":"2. 옵셔널 값 처리 문자열을 숫자로 변환해주는 생성자 Int(문자열) 이야기를 조금 더 해보자. 숫자로 바꿀 수 있는 문자열 \"123\"이 입력되면 숫자로 변환할 수 있지만, 그럴 수 없는 일반 문자열이 입력되면 잠재적인 오류 가능성이 있다고 설명했다. 이 때문에 Int(문자열)은 그냥 정수가 아니라 옵셔널 타입의 정수값을 반환하도록 설계되어 있다. Int 구조체의 생성자를...","categories": ["Swift-Grammar"],
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","옵셔널","Optional","언래핑","unwrapping"],
+        "url": "https://sanghyuk.dev/swift-grammar/6-2/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
+      },{
+        "title": "CHAPTER07: 함수 - 1.함수의 기본 개념",
+        "excerpt":"함수(Function)는 대부분의 프로그래밍 언어에서 지원하는 개념으로 프로그램의 실행 과정 중에서 독립적으로 처리될 수 있는 부분을 분리하여 구조화한 객체를 의미한다. 즉, 외부에 의존하는 부분 없이 툭 떼어 분리할 수 있는 실행 단위를 일종의 캡슐처럼 포장해놓은 것이다. 이렇게 독립적으로 작성된 함수는 간단한 방식으로 여러 번 호출하여 사용할 수 있어서 같은 코드를 반복해서...","categories": ["Swift-Grammar"],
+        "tags": ["Swift","Swift-Grammar","꼼곰한 재은 씨의 스위프트:문법편","이재은","루비페이퍼","ISBN:979-11-86710-23-4","함수","문법"],
+        "url": "https://sanghyuk.dev/swift-grammar/7-1/",
+        "teaser": "https://sanghyuk.dev/assets/images/posts/Swift-Grammar/swift.png"
+      },{
+        "title": "기획",
+        "excerpt":"제목   내용  ","categories": ["Planner"],
+        "tags": ["기획"],
+        "url": "https://sanghyuk.dev/planner/1/",
+        "teaser": null
       }]
